4341f50cfaeb1a8a377b229e50cb61db6a93ac00
YuriiTrofimenko <tyaa@ukr.net>
2022-05-26 18:41:35 +0300

    db connection setup, create_table migration: requests

---
+++ database/migrations/2022_05_26_150152_create_tables.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('email', 50);
+            $table->enum('status', ['Active', 'Resolved'])->default('Active');
+            $table->longText('message');
+            $table->longText('comment')->nullable();;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+};
